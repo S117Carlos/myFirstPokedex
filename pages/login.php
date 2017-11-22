ccbfc5ff782df5540ef313b6af86810383f37987
reach.117.war@gmail.com <reach.117.war@gmail.com>
2017-11-21 20:45:23 -0600

    Add animation and styles to the login page

--- pages/login.php
+++ pages/login.php
@@ -8,33 +8,42 @@
 	<link rel="stylesheet" type="text/css" href="../content/css/login.css">
 </head>
 <body>
-	<section>
-    <div class="css-tab tab-header-active">
-      <ul class="tab-list">
-        <li class="tab-item">
-          <a href="" class="tab-link">
-          <div class="tab-thumb color1" data-letter="H"></div>
-            <p class="tab-label">Home</p>
-          </a>
-        </li>
-      <li class="tab-item">
-        <a href="" class="tab-link">
-          <div class="tab-thumb color2" data-letter="C"></div>
-          <p class="tab-label">CSS</p>
-        </a>
-      </li>
-  </ul>
-</div>
-<div class="tab-content">
-  <section class="section tab-active color1" data-letter="a">
-    <article class="tab-wrapper">
-    </article>
-  </section>
-  <section class="section color2" data-letter="p">
-    <article class="tab-wrapper">
-    </article>
-  </section>
-</div>
+	<section class="container">
+		<section class="form-container">
+			<div class="css-tab tab-header-active">
+				<ul class="tab-list">
+					<li class="tab-item">
+						<a href="" class="tab-link">
+						<div class="tab-thumb color1" data-letter="H"></div>
+							<p class="tab-label">Sign In</p>
+						</a>
+					</li>
+				<li class="tab-item">
+					<a href="" class="tab-link">
+						<div class="tab-thumb color2" data-letter="C"></div>
+						<p class="tab-label">Register</p>
+					</a>
+				</li>
+		</ul>
+			</div>
+			<div class="tab-content">
+			<section class="section tab-active" data-letter="a">
+				<article class="tab-wrapper">
+					<form>
+						<div class="form-group">
+							<label>Username</label>
+							<input type="text">
+						</div>
+					</form>
+				</article>
+			</section>
+			<section class="section " data-letter="p">
+				<article class="tab-wrapper">
+				</article>
+			</section>
+			</div>
+	</section>
+
 	</section>
   <script src="../js/login.js"></script>
 </body>
